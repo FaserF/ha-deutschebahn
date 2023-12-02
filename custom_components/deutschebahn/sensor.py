f34be188315827465c5d42ad2537c21858ae978a
Green-Kite <36706555+Green-Kite@users.noreply.github.com>
2023-12-02 08:10:11 +0100

    update info for following trains
    added next_arrival, next_arrival_delay, next_on_arrival and next_on_arrival_delay to use with lovelace card

--- custom_components/deutschebahn/sensor.py
+++ custom_components/deutschebahn/sensor.py
@@ -106,10 +106,14 @@ class DeutscheBahnSensor(SensorEntity):
             if len(self.connections) > 1:
                 connections["next"] = self.connections[1]["departure"]
                 connections["next_delay"] = self.connections[1]["delay"]
+                connections["next_arrival"] = self.connections[1]["arrival"]
+                connections["next_arrival_delay"] = self.connections[2]["delay_arrival"]
                 connections["next_canceled"] = self.connections[1]["canceled"]
             if len(self.connections) > 2:
                 connections["next_on"] = self.connections[2]["departure"]
                 connections["next_on_delay"] = self.connections[2]["delay"]
+                connections["next_on_arrival"] = self.connections[2]["arrival"]
+                connections["next_on_arrival_delay"] = self.connections[2]["delay_arrival"]
                 connections["next_on_canceled"] = self.connections[2]["canceled"]
         else:
             connections = None
